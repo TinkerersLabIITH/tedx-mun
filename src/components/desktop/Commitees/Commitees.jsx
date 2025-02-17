74d47025e0ce99e1c2f5d6208fd7dd0596454878
BhuminHirpara22 <bhuminhirpara22@gmail.com>
2025-02-17 20:35:25 +0530

    New changes

--- src/components/desktop/Commitees/Commitees.jsx
+++ src/components/desktop/Commitees/Commitees.jsx
@@ -1,71 +1,41 @@
 import React from "react";
 import "./Commitees.css";
-import wto from "../../../assets/wto.png";
-import unsc from "../../../assets/unsc.png";
-import who from "../../../assets/who.png";
-import unicef from "../../../assets/unicef.png";
-import unesco from "../../../assets/unesco.png";
-import imf from "../../../assets/imf.png";
-import unhrc from "../../../assets/unhrc.png";
-import fao from "../../../assets/fao.png";
-import iaea from "../../../assets/iaea.png";
-import logo from "../../../assets/logo.png";
+import cardB from "../../../assets/cardB.png"
+import logo1 from "../../../assets/logo1.png";
+import logo2 from "../../../assets/logo2.png";
+import logo3 from "../../../assets/logo3.png";
 
 const committees = [
   {
-    id: 1,
-    title: "WTO",
-    description: 'We believe that for humankind to truly progress...',
-    background: wto,
-  },
-  {
-    id: 2,
-    title: "UNSC",
-    description: 'We believe that for humankind to truly progress...',
-    background: unsc,
-  },
-  {
-    id: 3,
-    title: "WHO",
-    description: 'We believe that for humankind to truly progress...',
-    background: who,
-  },
-  {
-    id: 4,
-    title: "UNICEF",
-    description: 'We believe that for humankind to truly progress...',
-    background: unicef,
-  },
-  {
-    id: 5,
-    title: "UNESCO",
-    description: 'We believe that for humankind to truly progress...',
-    background: unesco,
-  },
-  {
-    id: 6,
-    title: "IMF",
-    description: 'We believe that for humankind to truly progress...',
-    background: imf,
-  },
-  {
-    id: 7,
-    title: "UNHRC",
-    description: 'We believe that for humankind to truly progress...',
-    background: unhrc,
-  },
-  {
-    id: 8,
-    title: "FAO",
-    description: 'We believe that for humankind to truly progress...',
-    background: fao,
-  },
-  {
-    id: 9,
-    title: "IAEA",
-    description: `We believe that for humankind to truly progress...`,
-    background: iaea,
-  },
+      id: 1,
+      title: "UNSC",
+      description: 'We believe that for humankind to truly progress...',
+      logo: logo3,
+    },
+    {
+      id: 2,
+      title: "UNHRC",
+      description: 'We believe that for humankind to truly progress...',
+      logo: logo1,
+    },
+    {
+      id: 3,
+      title: "UNGA",
+      description: 'We believe that for humankind to truly progress...',
+      logo: logo3,
+    },
+    {
+      id: 4,
+      title: "UNESCO",
+      description: 'We believe that for humankind to truly progress...',
+      logo: logo2,
+    },
+    {
+      id: 5,
+      title: `UNSC Historic`,
+      description: 'We believe that for humankind to truly progress...',
+      logo: logo3,
+    },
 ];
 
 function Committees() {
@@ -73,25 +43,19 @@ function Committees() {
     <div className="Committees" style={{margin:"auto", marginTop:"-5vw"}}>
       {/* Committees Section */}
       <section id="committees" className="committees-section">
-        <h2 className="committee-heading" style={{fontSize:"3vw",fontWeight:"900"}}>COMMITTEES</h2>
-        <p className="description">
-          <span style={{fontWeight:"900"}}>"Ideas Worth Sharing: "</span>We believe that for humankind to truly progress and achieve our
-          potential, the exchange of ideas between people is essential. TED helps us list the problems
-          people face even in the remotest corners of the world. This promotes discussions to come up
-          with solutions and ideas from pioneers and amateurs.
-        </p>
-        <div className="cards-container">
+        <h2 className="committee-heading" style={{fontSize:"2.75vw",fontWeight:"900",marginTop:"5vw"}}>COMMITTEES</h2>
+        <h3 className="committee-heading" style={{fontSize:"1.2vw",fontWeight:"200",marginTop:"-2.5vw"}}>Agendas yet to be released.</h3>
+        <div className="cards-container" style={{gap:"10px"}}>
           {committees.map((committee) => (
             <div
               key={committee.id}
               className="card"
-              style={{ backgroundImage: `url(${committee.background})` }}
+              style={{ backgroundImage: `url(${cardB})`,width:"400px",height:"270px",marginTop:"60px" }}
             >
-              <div className="icon">
-                <img src={logo} alt={`${committee.title} logo`} />
+              <div className="icon" style={{width:"250px",height:"250px"}}>
+                <img src={committee.logo} alt={`${committee.title} logo`} style={{width:"200px",heigth:"200px"}}/>
               </div>
-              <h3>{committee.title}</h3>
-              <p><span style={{fontWeight:"900"}}>"Ideas Worth Sharing: "</span>{committee.description}</p>
+              <h3 style={{marginTop:"160px",fontSize:"2.75vw",fontWeight:"900"}}>{committee.title}</h3>
             </div>
           ))}
         </div>
--- src/components/mobile/Commitees/Commitees.jsx
+++ src/components/mobile/Commitees/Commitees.jsx
@@ -1,97 +1,59 @@
 import React from "react";
 import "./Commitees.css";
-import wto from "../../../assets/wto.png";
-import unsc from "../../../assets/unsc.png";
-import who from "../../../assets/who.png";
-import unicef from "../../../assets/unicef.png";
-import unesco from "../../../assets/unesco.png";
-import imf from "../../../assets/imf.png";
-import unhrc from "../../../assets/unhrc.png";
-import fao from "../../../assets/fao.png";
-import iaea from "../../../assets/iaea.png";
-import logo from "../../../assets/logo.png";
+import cardB from "../../../assets/cardB.png"
+import logo1 from "../../../assets/logo1.png";
+import logo2 from "../../../assets/logo2.png";
+import logo3 from "../../../assets/logo3.png";
 
 const committees = [
   {
-    id: 1,
-    title: "WTO",
-    description: 'We believe that for humankind to truly progress...',
-    background: wto,
-  },
-  {
-    id: 2,
-    title: "UNSC",
-    description: 'We believe that for humankind to truly progress...',
-    background: unsc,
-  },
-  {
-    id: 3,
-    title: "WHO",
-    description: 'We believe that for humankind to truly progress...',
-    background: who,
-  },
-  {
-    id: 4,
-    title: "UNICEF",
-    description: 'We believe that for humankind to truly progress...',
-    background: unicef,
-  },
-  {
-    id: 5,
-    title: "UNESCO",
-    description: 'We believe that for humankind to truly progress...',
-    background: unesco,
-  },
-  {
-    id: 6,
-    title: "IMF",
-    description: 'We believe that for humankind to truly progress...',
-    background: imf,
-  },
-  {
-    id: 7,
-    title: "UNHRC",
-    description: 'We believe that for humankind to truly progress...',
-    background: unhrc,
-  },
-  {
-    id: 8,
-    title: "FAO",
-    description: 'We believe that for humankind to truly progress...',
-    background: fao,
-  },
-  {
-    id: 9,
-    title: "IAEA",
-    description: `We believe that for humankind to truly progress...`,
-    background: iaea,
-  },
+      id: 1,
+      title: "UNSC",
+      description: 'We believe that for humankind to truly progress...',
+      logo: logo3,
+    },
+    {
+      id: 2,
+      title: "UNHRC",
+      description: 'We believe that for humankind to truly progress...',
+      logo: logo1,
+    },
+    {
+      id: 3,
+      title: "UNGA",
+      description: 'We believe that for humankind to truly progress...',
+      logo: logo3,
+    },
+    {
+      id: 4,
+      title: "UNESCO",
+      description: 'We believe that for humankind to truly progress...',
+      logo: logo2,
+    },
+    {
+      id: 5,
+      title: `UNSC Historic`,
+      description: 'We believe that for humankind to truly progress...',
+      logo: logo3,
+    },
 ];
-
 function MobileCommittees() {
   return (
     <div className="Committees" style={{margin:"auto", marginTop:"-5vw"}}>
       {/* Committees Section */}
       <section id="committees" className="committees-section">
         <h2 className="committee-heading" style={{fontSize:"3vh",fontWeight:"900"}}>COMMITTEES</h2>
-        <p className="description" style={{fontSize:"16px"}}>
-          <span style={{fontWeight:"900"}}>"Ideas Worth Sharing": </span>We believe that for humankind to truly progress and achieve our
-          potential, the exchange of ideas between people is essential. TED helps us list the problems
-          people face even in the remotest corners of the world. This promotes discussions to come up
-          with solutions and ideas from pioneers and amateurs.
-        </p>
         <div className="cards-container" style={{alignItems: "center"}}>
           {committees.map((committee) => (
             <div
               key={committee.id}
               className="card"
-              style={{ backgroundImage: `url(${committee.background})`, margin: "20% 0" }}
+              style={{ backgroundImage: `url(${cardB})`, margin: "20% 0",height:"250px" }}
             >
               <div className="icon">
-                <img src={logo} alt={`${committee.title} logo`} />
+                <img src={committee.logo} alt={`${committee.title} logo`} />
               </div>
               <h3 style={{fontSize: "32px"}}>{committee.title}</h3>
-              <p style={{fontSize: "16px"}}><span style={{fontWeight:"900"}}>"Ideas Worth Sharing: "</span>{committee.description}</p>
             </div>
           ))}
         </div>
